d0cda800cdf032512692e12d9074ab56dc9fe215
daniel.colina <daniel.colina@dotcms.com>
2023-10-19 21:10:19 +0200

    (feat): New version testing.

--- src/main/java/com/dotcms/GreetingCommand.java
+++ src/main/java/com/dotcms/GreetingCommand.java
@@ -1,29 +1,20 @@
 package com.dotcms;
 
-import jakarta.enterprise.context.Dependent;
 import picocli.CommandLine;
 
 @CommandLine.Command
 public class GreetingCommand implements Runnable {
 
+    public static void main(String[] args) {
+        CommandLine.run(new GreetingCommand(), args);
+    }
+
     @CommandLine.Option(names = {"-n", "--name"}, description = "Who will we greet?", defaultValue = "World")
     String name;
 
-    private final GreetingService greetingService;
-
-    public GreetingCommand(GreetingService greetingService) {
-        this.greetingService = greetingService;
-    }
-
     @Override
     public void run() {
-        greetingService.sayHello(name);
-    }
-}
-
-@Dependent
-class GreetingService {
-    void sayHello(String name) {
         System.out.println("Hello " + name + "!");
     }
 }
+
